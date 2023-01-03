207634297fcfeadb7b4c4c4fee9e9d8ea9e5052d
Jasmeher <bharadwajjasmeher@gmail.com>
2023-01-03 10:38:47 +0530

    stripe integration

--- server.js
+++ server.js
@@ -1,17 +1,45 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
-const dotenv = require("dotenv");
-dotenv.config({ path: "./.env" });
 const connection = require("./config/connection");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+app.use(express.static(process.env.STATIC_DIR));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.get("/", function (req, res) {
   res.send(`Server is listening on port ${process.env.PORT}`);
 });
+app.get("/config", (req, res) => {
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: "inr",
+      amount: 1900,
+      description: "Testing",
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+
+    res.send({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    return res.status(400).json({
+      error: {
+        message: error.message,
+      },
+    });
+  }
+});
+
 app.use("/auth", require("./routes/authRoutes"));
 app.use("/user", require("./routes/userRoutes"));
 app.use("/product", require("./routes/productRoutes"));
